50c3c65acf53db86204737af96244b30c8790b06
xami4ok <59232793+xami4ok@users.noreply.github.com>
2020-04-26 02:05:00 +0700

    Add files via upload

---
+++ emailo.php
@@ -0,0 +1,10 @@
+$nikname= $_POST['nikname'];
+$phone= $_POST['phone'];
+$pasta= $_POST['pasta'];
+$mail= $_POST['kurer'];
+$msg= $_POST['msg'];
+$emailTo = 'ily30014001@gmail.com';
+$body = "$nikname \n\n$phone \n\n$pasta \n\n$mail \n\n$msg";
+$headers = "Content-Type: text/plain; charset=utf-8\r\n".'From: MySite <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $emailTo;
+mail($emailTo, $nikname, $body, $headers);
+$emailSent = true;
